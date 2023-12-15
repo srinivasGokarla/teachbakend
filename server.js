80e0a675f1be8f7a574064fa6e6362748f5c860d
srinivasGokarla <srinivasgokarla@gmail.com>
2023-12-15 19:42:08 +0530

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const authRoutes = require('./routes/userRoutes');
+const authRoutes = require('./src/routes/userRoutes');
 const cors = require('cors');
 
 
